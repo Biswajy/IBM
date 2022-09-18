dbf5e0a654a9a73fd1e753cfc6719779c2c32ef3
Biswajyoti Pradhan <76693888+Biswajy@users.noreply.github.com>
2022-09-18 16:31:11 +0530

    Add files via upload

---
+++ IBM_Project.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# My Jupyter Notebook on IBM Watson Studio
+
+# Name : **Biswajyoti Pradhan**
+# 
+# Desired Ocupation : **Data Scientist**
+
+# *I am interested in data science because,data is the heart of any organisation. 10 to 15 years ago, we did not have this much of data that we are generating today.
+# The decission I make is always data driven and I always try to visualize the data so that I can make my decission clearly.
+# Moreover I can say crunching numbers, finding patterns in data, visualizing data and story telling,building,testing and deployiing ML models is a lot interesting and cchallenging.
+# Due to above said reason and a person who loves challenges,would love to be a data scientist.*
+
+# ### Below code explains that if the location is bank and Auto shop,it will show the respective print statement.Other than this if location is any,it will print 'where are you'?
+# ​
+
+# In[3]:
+
+
+loc = 'Bank'
+
+if loc == 'Auto Shop':
+    print('Welcome to the Auto Shop!')
+elif loc == 'Bank':
+    print('Welcome to the bank!')
+else:
+    print('Where are you?')
+
+
+# In[ ]:
+
+
+
+
